dbd109afcf2de58b4c5ba44fa2a84ad23dab56b8
keisato848 <72418986+keisato848@users.noreply.github.com>
2021-06-13 16:06:56 +0900

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <iostream> // 標準で提供されている入出力機能を有効するために必要なヘッダーファイルを読み込む
 
+// main()関数の定義
 int main() 
 {
-  std::cout << "Hello, world" << std::endl;
+  std::cout << "Hello, world" << std::endl << "everyone" << std::endl; // std::coutを左側に起き、左シフト演算子の右側に出力したい文字列を配置し、std::endlで改行される
+  std::cout << "Hello, c++" << std::endl;
 }
